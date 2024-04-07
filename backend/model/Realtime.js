f63c38c9002d7eca9f45be957accb4d7d9393dd3
shyamswaran <shyamswaran@gmail.com>
2024-04-07 21:51:35 +0530

    final2.0

--- backend/model/Realtime.js
+++ backend/model/Realtime.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const connection = mongoose.createConnection("mongodb://127.0.0.1:27017/emeter");
+const connection = mongoose.createConnection(process.env.MONGO_DB);
 const schema = new mongoose.Schema({
   _id: {
     type: Number,
